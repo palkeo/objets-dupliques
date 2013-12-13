ce902031d89e442add5f1672a859d192dff88f73
Maxime Arthaud <maxime.arthaud@gmail.com>
2013-12-13 20:00:46 +0100

    Avancement du client.

--- src/Client.java
+++ src/Client.java
@@ -3,14 +3,19 @@ import java.rmi.server.UnicastRemoteObject;
 import java.rmi.registry.*;
 import java.util.HashMap;
 import java.net.*;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class Client extends UnicastRemoteObject implements Client_itf
 {
     private static String RMI_PATH = "//localhost/SharedObjects";
-    
+
+    private static Client client;
+
     private static Server_itf server;
-    
-    private HashMap<Integer, SharedObject> objects;
+
+    private static HashMap<Integer, SharedObject> objects;
+
+    private static ReentrantLock mutex;
 
 	public Client() throws RemoteException
     {
@@ -24,32 +29,59 @@ public class Client extends UnicastRemoteObject implements Client_itf
 	// initialization of the client layer
 	public static void init()
     {
-	    try
-	    {
-			server = (Server_itf)Naming.lookup(RMI_PATH);
-		}
-	    catch (Exception e)
-	    {
-			throw new RuntimeException();
-		}
+        client = new Client();
+		server = (Server_itf)Naming.lookup(RMI_PATH);
 	}
 
 	// lookup in the name server
 	public static SharedObject lookup(String name)
     {
-	    // id_objet = server.lookup(name)
-	    // return new SharedObject…
+        try
+        {
+            SharedObject so = new SharedObject(server.lookup(name));
+
+            mutex.lock();
+            objects.put(so.getId(), so);
+            mutex.unlock();
+
+            return so;
+        }
+        catch(RemoteException e)
+        {
+            throw new RuntimeException(name + " cannot be found");
+        }
 	}
 
 	// binding in the name server
-	public static void register(String name, SharedObject_itf so)
+	public static void register(String name, SharedObject so)
     {
+        try
+        {
+            server.register(name, so.getId());
+        }
+        catch(RemoteException e)
+        {
+            throw new RuntimeException("Erreur server.register");
+        }
 	}
 
 	// creation of a shared object
 	public static SharedObject create(Object o)
     {
+        try
+        {
+            SharedObject so = new SharedObject(server.create(o));
 
+            mutex.lock();
+            objects.put(so.getId(), so);
+            mutex.unlock();
+
+            return so;
+        }
+        catch(RemoteException e)
+        {
+            throw new RuntimeException("Erreur server.create");
+        }
 	}
 
 /////////////////////////////////////////////////////////////
@@ -59,25 +91,62 @@ public class Client extends UnicastRemoteObject implements Client_itf
 	// request a read lock from the server
 	public static Object lock_read(int id)
     {
+        try
+        {
+            server.lock_read(id, client);
+        }
+        catch(RemoteException e)
+        {
+            throw new RuntimeException(e.toString());
+        }
 	}
 
 	// request a write lock from the server
 	public static Object lock_write (int id)
     {
+        try
+        {
+            server.lock_write(id, client);
+        }
+        catch(RemoteException e)
+        {
+            throw new RuntimeException(e.toString());
+        }
 	}
 
 	// receive a lock reduction request from the server
 	public Object reduce_lock(int id) throws java.rmi.RemoteException
     {
+        SharedObject so;
+
+        mutex.lock();
+        so = objects.get(id);
+        mutex.unlock();
+
+        return so.reduce_lock();
 	}
 
 	// receive a reader invalidation request from the server
 	public void invalidate_reader(int id) throws java.rmi.RemoteException
     {
+        SharedObject so;
+
+        mutex.lock();
+        so = objects.get(id);
+        mutex.unlock();
+
+        so.invalidate_reader();
 	}
 
 	// receive a writer invalidation request from the server
 	public Object invalidate_writer(int id) throws java.rmi.RemoteException
     {
+        SharedObject so;
+
+        mutex.lock();
+        so = objects.get(id);
+        mutex.unlock();
+
+        return so.invalidate_writer();
 	}
 }
--- src/SharedObject.java
+++ src/SharedObject.java
@@ -13,19 +13,22 @@ public class SharedObject implements Serializable, SharedObject_itf
         RLT_WLC,
     };
 
-	private Client_itf client;
 	private int id;
 	private Object obj;
 	private State state;
 
-	SharedObject(Client_itf client, int id)
+	SharedObject(int id)
 	{
 	    this.state = State.NL;
-		this.client = client;
 		this.id = id;
 		this.obj = null;
 	}
 	
+    public int getId()
+    {
+        return this.id;
+    }
+
 	public Object getObj()
 	{
 	    return this.obj;
@@ -34,29 +37,100 @@ public class SharedObject implements Serializable, SharedObject_itf
 	// invoked by the user program on the client node
 	public void lock_read()
     {
+        if(this.state == State.NL)
+        {
+            Client.lock_read(this.id);
+            this.state = State.RLT;
+        }
+        else if(this.state == State.RLC)
+        {
+            this.state = State.RLT;
+        }
+        else if(this.state == State.WLC)
+        {
+            this.state = State.RLT_WLC;
+        }
 	}
 
 	// invoked by the user program on the client node
 	public void lock_write()
     {
+        if(this.state == State.NL || this.state == State.RLC || this.state == State.RLT)
+        {
+            Client.lock_write(this.id);
+        }
+
+        this.state = State.WLT;
 	}
 
 	// invoked by the user program on the client node
 	public synchronized void unlock()
     {
+        if(this.state == State.RLT)
+        {
+            this.state = State.RLC;
+        }
+        else if(this.state == State.WLT || this.state == State.RLT_WLC)
+        {
+            this.state = State.WLC;
+        }
 	}
 
 	// callback invoked remotely by the server
 	public synchronized Object reduce_lock()
     {
+        try
+        {
+            while(this.state == State.WLT)
+                Thread.sleep(1); // FIXME
+
+            if(this.state == State.WLC)
+            {
+                this.state = State.RLC;
+            }
+            else if(this.state == State.RLT_WLC)
+            {
+                this.state = State.RLT;
+            }
+
+            return this.obj;
+        }
+        catch(InterruptedException e)
+        {
+            throw new RuntimeException(e.toString());
+        }
 	}
 
 	// callback invoked remotely by the server
 	public synchronized void invalidate_reader()
     {
+        try
+        {
+            while(this.state == State.RLT || this.state == State.RLT_WLC)
+                Thread.sleep(1); // FIXME
+
+            this.state = State.NL;
+        }
+        catch(InterruptedException e)
+        {
+            throw new RuntimeException(e.toString());
+        }
 	}
 
 	public synchronized Object invalidate_writer()
     {
+        try
+        {
+            while(this.state == State.WLT || this.state == State.RLT || this.state == State.RLT_WLC)
+                Thread.sleep(1); // FIXME
+
+            this.state = State.NL;
+
+            return this.obj;
+        }
+        catch(InterruptedException e)
+        {
+            throw new RuntimeException(e.toString());
+        }
 	}
 }
