a98e7dba1d9c19a600e6c4e1976cc71e8a5d94cb
palkeo <contact@palkeo.com>
2013-12-18 00:59:04 +0100

    SharedObject avec mutexes.

--- src/SharedObject.java
+++ src/SharedObject.java
@@ -5,6 +5,8 @@ public class SharedObject implements Serializable, SharedObject_itf
 {
 	private static final long serialVersionUID = 8271196138090195418L;
 
+    private ReentrantLock mutex = new ReentrantLock();
+
     private enum State {
         NL,
         RLC,
@@ -42,9 +44,12 @@ public class SharedObject implements Serializable, SharedObject_itf
 	// invoked by the user program on the client node
 	public void lock_read()
     {
+        mutex.lock();
         if(this.state == State.NL)
         {
+            mutex.unlock();
             this.obj = Client.lock_read(this.id);
+            mutex.lock();
             this.state = State.RLT;
         }
         else if(this.state == State.RLC)
@@ -55,22 +60,28 @@ public class SharedObject implements Serializable, SharedObject_itf
         {
             this.state = State.RLT_WLC;
         }
+        mutex.unlock();
 	}
 
 	// invoked by the user program on the client node
 	public void lock_write()
     {
+        mutex.lock();
         if(this.state == State.NL || this.state == State.RLC || this.state == State.RLT)
         {
+            mutex.unlock();
             this.obj = Client.lock_write(this.id);
+            mutex.lock();
         }
 
         this.state = State.WLT;
+        mutex.unlock();
 	}
 
 	// invoked by the user program on the client node
 	public void unlock()
     {
+        mutex.lock();
         if(this.state == State.RLT)
         {
             this.state = State.RLC;
@@ -79,15 +90,19 @@ public class SharedObject implements Serializable, SharedObject_itf
         {
             this.state = State.WLC;
         }
+        mutex.unlock();
 	}
 
 	// callback invoked remotely by the server
-	public synchronized Object reduce_lock()
+	public /*synchronized*/ Object reduce_lock()
     {
+        mutex.lock();
         while(this.state == State.WLT)
         {
             // FIXME
+            mutex.unlock();
             Thread.yield();
+            mutex.lock();
         }
 
         if(this.state == State.WLC)
@@ -99,33 +114,42 @@ public class SharedObject implements Serializable, SharedObject_itf
             this.state = State.RLT;
         }
 
+        mutex.unlock();
         return this.obj;
 	}
 
 	// callback invoked remotely by the server
-	public synchronized void invalidate_reader()
+	public void /*synchronized*/ invalidate_reader()
     {
+        mutex.lock();
         while(this.state == State.RLT || this.state == State.RLT_WLC)
         {
             // FIXME
+            mutex.unlock();
             Thread.yield();
+            mutex.lock();
         }
 
         this.state = State.NL;
         this.obj = null; // to help debugging
+        mutex.unlock();
 	}
 
 	// callback invoked remotely by the server
-	public synchronized Object invalidate_writer()
+	public /*synchronized*/ Object invalidate_writer()
     {
+        mutex.lock();
         while(this.state == State.WLT || this.state == State.RLT || this.state == State.RLT_WLC)
         {
             // FIXME
+            mutex.unlock();
             Thread.yield();
+            mutex.lock();
         }
 
         this.state = State.NL;
 
+        mutex.unlock();
 
         return this.obj;
 	}
