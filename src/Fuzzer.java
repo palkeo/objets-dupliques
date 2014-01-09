1b5f92160b740e9ec5ac582bc119569ca9516e08
Maxime Arthaud <maxime.arthaud@gmail.com>
2014-01-09 21:13:51 +0100

    Suppression d'import et de commentaires inutiles

--- src/Fuzzer.java
+++ src/Fuzzer.java
@@ -1,11 +1,3 @@
-import java.awt.*;
-import java.awt.event.*;
-import java.rmi.*;
-import java.io.*;
-import java.net.*;
-import java.util.*;
-import java.lang.*;
-import java.rmi.registry.*;
 import java.util.UUID;
 
 
@@ -15,11 +7,8 @@ public class Fuzzer
 
 	public static void main(String argv[])
     {
-		// initialize the system
 		Client.init();
 
-		// look up the IRC object in the name server
-		// if not found, create it, and register it in the name server
 		SharedObject s = Client.lookup("IRC");
 		if (s == null)
         {
@@ -27,7 +16,6 @@ public class Fuzzer
 			Client.register("IRC", s);
 		}
 
-		// create the graphical part
 		new Fuzzer(s);
 	}
 
@@ -40,7 +28,7 @@ public class Fuzzer
             for(int i = 0; i < 100; i++)
             {
                 String s2 = ((Sentence)(so.obj)).read();
-                if((s != null && ! s.equals(s2)) || s2 == null)
+                if(s2 == null || (s != null && !s.equals(s2)))
                 {
                     throw new RuntimeException("Inconsistency detected.");
                 }
